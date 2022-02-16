5620f9615ab2f48bdcbb6420f9b779c48459c0b7
Niklas Hietala <niklas.hietala1997@gmail.com>
2022-02-16 22:28:32 +0200

    Controller change

--- src/main/java/com/example/BookStore/web/BookController.java
+++ src/main/java/com/example/BookStore/web/BookController.java
@@ -1,5 +1,15 @@
 package com.example.BookStore.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BookController {
+
+	@GetMapping("/index")
+	public String metodi() {
+
+		return "index";
+	}
 
 }
