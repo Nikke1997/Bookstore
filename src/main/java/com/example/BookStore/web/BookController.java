a77b81eb56d770c311072f34b5aac065bff8ea4f
Niklas Hietala <niklas.hietala1997@gmail.com>
2022-02-22 23:18:15 +0200

    Added add and delete button

--- src/main/java/com/example/BookStore/web/BookController.java
+++ src/main/java/com/example/BookStore/web/BookController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.BookStore.domain.Book;
 import com.example.BookStore.domain.BooksRepository;
 
 @Controller
@@ -16,6 +20,24 @@ public class BookController {
 	public String booklist(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
+	}
+
+	@GetMapping("/delete/{id}")
+	public String deletebook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "booklist";
+	}
+
+	@RequestMapping("/add")
+	public String addStudent(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+
+	@PostMapping("/save")
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
 	}
 
 }
